4c3399ef8f881528847e299d0042f8e8117c489e
saeedrafay <saeedahmadrafay@hotmail.com>
2021-05-06 01:06:19 +0200

    Order Screen and error fix in orderController

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -98,7 +98,7 @@ const PlaceOrderScreen = ({ history }) => {
                           </Col>
                           <Col md={4}>
                             {item.qty} x ${item.price} = $
-                            {addDecimals(item.qty * item.price)}
+                            {(item.qty * item.price).toFixed(2)}
                           </Col>
                         </Row>
                       </ListGroup.Item>
